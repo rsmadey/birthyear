8cd81c9ce3ac702107f68d14ece4deb7ad28ac3e
rsmadey <rsmadey88@gmail.com>
2017-02-15 09:29:48 -0600

    php function and usage split into multible files

---
+++ README.txt
@@ -0,0 +1,6 @@
+birthday.inc contains function for finding most frequent year
+
+for test run use 
+"php birthday.php test_file.txt"
+
+This should returnt 1987 as the most frequent year"
--- birthday.php
+++ birthday.php
@@ -1,27 +1,19 @@
-<?php
+<?php 
+include_once "birthday.inc";
 
-$birthyears = file('test_file.txt');
-$birthyears = array_map('trim',$birthyears);
-echo most_frequent($birthyears);
-
-
-//given array of birthdays counts ones between 1900 and 2000 inclusive
-//returns 0 if empty array or year with most birthdays
-function most_frequent($birthyears){
-	$count = 0;
-	$year = 0;
-
-	$possible_birthdays = array_fill(1900,101,0);
-	foreach($birthyears as $x => $birthyear){
-		if($birthyear >=1900 && $birthyear <= 2000){
-			$possible_birthdays[$birthyear] += 1  ;//= $possible_birthdays[$birthyear] + 1;
-		}
-		if($possible_birthdays[$birthyear] > $count){
-			$count = $possible_birthdays[$birthyear];
-			$year = $birthyear;
-		}
+if($argc != 2){
+	echo "Usage birthday.php {file}\n";
+}else{
+	$birthyears = file($argv[1]);
+	$birthyears = array_map('trim',$birthyears);
+	$year = most_frequent($birthyears);
+	if($year === 0) {
+		echo "inaccurate data in file\n";
+	}else{
+		echo "most frequent birthyear is: $year\n";
 	}
-	return $year;
+
 }
+
 
 ?>
